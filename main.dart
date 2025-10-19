578b4633e42aebf91a746aaeb8127c8275ed3099
codewithowais <codewithowais@gmail.com>
2025-10-19 13:59:40 +0500

    first commit basic if else

---
+++ main.dart
@@ -0,0 +1,24 @@
+void main() {
+  int a = 10;
+  int b = 20;
+
+  print(!(2 > 3) || (7 > 8));
+
+  if (!(a > b)) {
+    print("Ammi mm dahi lay aya");
+//   } else if() {
+
+  } else {
+    print("Ammi mm dahi lay aya but agy wali se");
+  }
+
+
+  /**
+   * marks > 90 => A+
+   * marks > 80 => A
+   * marks > 70 => B
+   * marks > 60 => C
+   * marks < 50 => F
+   * 
+   */
+}
